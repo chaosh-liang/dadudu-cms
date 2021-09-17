f07ab1fb3c92a0cacb2c4c77cd38d03706b603ca
chaosh-liang <chao.liang.fly@gmail.com>
2021-09-17 12:45:43 +0800

    chore: craco 配置中，暂时不设置 dll 和 optimization

--- craco.config.js
+++ craco.config.js
@@ -2,23 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const { DllReferencePlugin } = require('webpack');
 const { whenProd, whenDev } = require('@craco/craco');
-const TerserPlugin = require('terser-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
+// const safePostCssParser = require('postcss-safe-parser');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 // 判断编译环境是否为生产
 // const isBuildAnalyzer = process.env.REACT_APP_BUILD_ANALYZER === 'true';
 
 // console.log('打包 env => ', process.env.NODE_ENV);
 
+// const isEnvProduction = process.env.NODE_ENV === 'production';
+// const isEnvDevelopment = process.env.NODE_ENV === 'development';
+
 // 代理，默认配置
 let proxy = {
   '/api': {
     // url 会自动补全：`${target}/api`
-    target: 'http://localhost:7716/dadudu', // 本地服务
-    // target: 'http://101.34.21.222/dadudu', // 线上服务
+    target: 'http://localhost:7716/cms/dadudu', // 本地服务
+    // target: 'http://101.34.21.222/cms/dadudu', // 线上服务
     secure: false,
     changeOrigin: true,
   },
@@ -31,7 +36,7 @@ try {
 } catch (error) {}
 
 // dll 相关
-const handleDllPlugins = () => {
+/* const handleDllPlugins = () => {
   const dllSuffix = process.env.NODE_ENV === 'production' ? 'pro' : 'dev';
   const files = fs.readdirSync(path.resolve(__dirname, `dll_${dllSuffix}`));
 
@@ -52,7 +57,7 @@ const handleDllPlugins = () => {
       return null;
     })
     .filter((i) => i);
-};
+}; */
 
 module.exports = {
   devServer: {
@@ -78,21 +83,98 @@ module.exports = {
         ],
         []
       ),
-      ...handleDllPlugins(),
-
-      /* ...whenProd(() => [
+      // ...handleDllPlugins(), // TODO
+    ],
+    /* optimization: {
+      minimize: isEnvProduction,
+      minimizer: [
+        // This is only used in production mode
         new TerserPlugin({
           terserOptions: {
+            parse: {
+              ecma: 8,
+            },
             compress: {
-              drop_console: true
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+              drop_debugger: true,
+              drop_console: true,
+            },
+            mangle: {
+              safari10: true,
             },
             output: {
-              comments: false
-            }
-          }
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          },
         }),
-      ], []) */
-    ],
+        // This is only used in production mode
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            parser: safePostCssParser,
+          },
+          cssProcessorPluginOptions: {
+            preset: [
+              'default',
+              {
+                discardComments: { removeAll: true },
+                minifyFontValues: { removeQuotes: false },
+              },
+            ],
+          },
+        }),
+      ],
+      splitChunks: {
+        name: true,
+        chunks: 'all',
+        minSize: 200000, // chunk 大于 20k 才会被拆分
+        maxAsyncRequests: 9, // 最大异步请求数
+        maxInitialRequests: 6, // 页面初始化最大异步请求数
+        automaticNameDelimiter: '-', // 解决命名冲突
+        cacheGroups: {
+          common: {
+            name: 'chunk-common',
+            chunks: 'all',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 1,
+          },
+          antd: {
+            name: 'chunk-antd',
+            chunks: 'all',
+            test: /[\\/]node_modules[\\/](@ant-design|antd)[\\/]/,
+            priority: 2,
+          },
+          echarts: {
+            name: 'chunk-echarts',
+            chunks: 'all',
+            test: /[\\/]node_modules[\\/]echarts[\\/]/,
+            priority: 3,
+          },
+          lodash: {
+            name: 'chunk-lodash',
+            test: /[\\/]node_modules[\\/]lodash[\\/]/,
+            priority: 4,
+            chunks: 'all',
+          },
+          axios: {
+            name: 'chunk-axios',
+            test: /[\\/]node_modules[\\/]axios[\\/]/,
+            priority: 5,
+            chunks: 'all',
+          },
+          moment: {
+            name: 'chunk-moment',
+            test: /[\\/]node_modules[\\/]moment[\\/]/,
+            priority: 6,
+            chunks: 'all',
+          },
+        },
+      },
+    }, */
     configure: (webpackConfig, { env, paths }) => {
       paths.appBuild = 'dist';
       webpackConfig.devtool = whenProd(() => false, webpackConfig.devtool);
@@ -103,72 +185,7 @@ module.exports = {
         filename: 'js/[id].bundle.[hash:4].js',
         chunkFilename: 'js/[id].[chunkhash:6].js',
       };
-
-      webpackConfig.optimization.minimizer.map((plugin) => {
-        if (plugin instanceof TerserPlugin) {
-          Object.assign(plugin.options.terserOptions.compress, {
-            drop_debugger: whenProd(() => true, false),
-            drop_console: whenProd(() => true, false),
-          });
-          Object.assign(plugin.options.terserOptions.output, {
-            comments: whenProd(() => false, true),
-          });
-        }
-
-        return plugin;
-      });
-
-      webpackConfig.optimization.splitChunks = {
-        ...webpackConfig.optimization.splitChunks,
-        ...{
-          chunks: 'all',
-          minSize: 200000, // chunk 大于 20k 才会被拆分
-          maxAsyncRequests: 9, // 最大异步请求数
-          maxInitialRequests: 6, // 页面初始化最大异步请求数
-          automaticNameDelimiter: '-', // 解决命名冲突
-          // name: true值将会自动根据切割之前的代码块和缓存组键值(key)自动分配命名,否则就需要传入一个String或者function.
-          name: true,
-          cacheGroups: {
-            common: {
-              name: 'chunk-common',
-              chunks: 'all',
-              test: /[\\/]node_modules[\\/]/,
-              priority: 1,
-            },
-            antd: {
-              name: 'chunk-antd',
-              chunks: 'all',
-              test: /[\\/]node_modules[\\/](@ant-design|antd)[\\/]/,
-              priority: 2,
-            },
-            echarts: {
-              name: 'chunk-echarts',
-              chunks: 'all',
-              test: /[\\/]node_modules[\\/]echarts[\\/]/,
-              priority: 3,
-            },
-            lodash: {
-              name: 'chunk-lodash',
-              test: /[\\/]node_modules[\\/]lodash[\\/]/,
-              priority: 4,
-              chunks: 'all',
-            },
-            axios: {
-              name: 'chunk-axios',
-              test: /[\\/]node_modules[\\/]axios[\\/]/,
-              priority: 5,
-              chunks: 'all',
-            },
-            moment: {
-              name: 'chunk-moment',
-              test: /[\\/]node_modules[\\/]moment[\\/]/,
-              priority: 6,
-              chunks: 'all',
-            },
-          },
-        },
-      };
-      // console.dir(webpackConfig);
+      // console.dir(webpackConfig.optimization.splitChunks);
       // console.log('环境 => ', env, paths);
       return webpackConfig;
     },
